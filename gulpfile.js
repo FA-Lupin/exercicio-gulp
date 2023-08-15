3656f19576a0d20308dd73d0ac88caf77a971dfc
Felipe Augusto <workcontact.dev@gmail.com>
2023-08-14 22:34:27 -0300

    two commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,31 @@ const uglify = require('gulp-uglify');
 const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin');
 
-function comprimeImagens(){
-    return gulp.src('.source/image/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/images'));
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
 }
 
-function comprimeJavaScript (){
+function comprimeJavaScript() {
     return gulp.src('./source/scripts/*.js')
-    .pipe(uglify())
-    .pipe(obfuscate())
-    .pipe(gulp.dest('./build/scripts'))
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'));
 }
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'compressed'
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
         }))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/styles'));
 }
 
 exports.default = function() {
-    gulp.watch('./source/style/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
-    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
-    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
-}
-
-
+    gulp.watch('./source/styles/**/*.scss', { ignoreInitial: false }, compilaSass);
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, comprimeJavaScript);
+    gulp.watch('./source/images/*', { ignoreInitial: false }, comprimeImagens);
+};
